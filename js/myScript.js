2201976095ee8e0e8cc1bf9c982669ba037816f7
Vinterqvist <55393216+Vinterqvist@users.noreply.github.com>
2019-10-24 10:31:27 +0200

    uppladdattest

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,6 @@
-var dataUrl = "https://openstreetws.stockholm.se/LvWS-3.0/Lv.svc/json/GetDataCatalog?apikey=2298bee4-b929-4a09-af4c-b893b3181902&FeatureTypeName=Belysningsmontage"
+var dataUrl = "https://openstreetws.stockholm.se/LvWS-3.0/Lv.svc"
+
+DataCatalog?apikey=2298bee4-b929-4a09-af4c-b893b3181902&FeatureTypeName=Belysningsmontage"
 
 
 d3.json(dataUrl).then(function(d) {
