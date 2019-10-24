8b0ecdae47fb7ce6221f1390d92a6c984dc26c60
Vinterqvist <55393216+Vinterqvist@users.noreply.github.com>
2019-10-24 11:55:41 +0200

    uppdaterad nude

--- js/myScript.js
+++ js/myScript.js
@@ -1,10 +1,41 @@
-var dataUrl = "https://openstreetws.stockholm.se/LvWS-3.0/Lv.svc"
+$("body").append("<img src='https://i.pinimg.com/originals/a7/a4/8c/a7a48c091299b55edf76ee6586f90d4e.jpg' />"); 
 
-DataCatalog?apikey=2298bee4-b929-4a09-af4c-b893b3181902&FeatureTypeName=Belysningsmontage"
+imgList =  ["imglink", "https://cdn-prod.medicalnewstoday.com/content/images/articles/274/274620/two-peaches.jpg", ]; 
 
 
-d3.json(dataUrl).then(function(d) {
-
-console.log(d);
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://nuditysearch.p.rapidapi.com/nuditySearch/image",
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-host": "nuditysearch.p.rapidapi.com",
+		"x-rapidapi-key": "81672fc9demshf0d5ebd8607d0b1p12913ejsnd4cac86f3d79",
+		"content-type": "application/x-www-form-urlencoded"
+	},
+	"data": {
+		"setting": "3",
+		"objecturl": "https://i.pinimg.com/originals/a7/a4/8c/a7a48c091299b55edf76ee6586f90d4e.jpg"
+	}
 }
-); 
+
+
+// loopa listan av alla bilder 
+// för alla bilder settings.objecturl = imgList[index];
+// lägga in aktuell bild
+// testa för bild 
+// visa upp resultat i html
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    console.log(response.score);
+ 
+    if (response.score > 30) {
+        $("body").append("<span> Nude! </span>"); 
+    } else {
+        $("body").append("<span> Not Nude! </span>"); 
+    }
+
+});
+
+
