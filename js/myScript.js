a9242bd876af5df0618b149cfde0c10281458a94
plix <palle.torsson@gmail.com>
2019-10-28 10:45:36 +0100

    old update

--- js/myScript.js
+++ js/myScript.js
@@ -1,8 +1,10 @@
-var dataUrl = "https://openstreetws.stockholm.se/LvWS-3.0/Lv.svc/json/GetDataCatalog?apikey=2298bee4-b929-4a09-af4c-b893b3181902&FeatureTypeName=Belysningsmontage"
-
-
+var dataUrl = "./js/dataCy.json";
+var apikey = "2298bee4-b929-4a09-af4c-b893b3181902";
+var what = "Cykelpump_Punkt";
+var baseUrl = "https://openstreetws.stockholm.se/LvWS-3.0/Lv.svc/json/GetDataCatalog?apikey=2298bee4-b929-4a09-af4c-b893b3181902&FeatureTypeName=";
+var searchUrl = baseUrl+what;
+console.log(searchUrl);
 d3.json(dataUrl).then(function(d) {
-
-console.log(d);
+  console.log(d);
 }
-); 
+);
